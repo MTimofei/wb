9a17b022646f6b104d73761606ff8fe38451703f
MTimofei <work.tim@rambler.ru>
2023-07-20 20:59:24 +0300

    create test db and test she

---
+++ cmd/0L/internal/database/testdb/testdb.go
@@ -0,0 +1,30 @@
+package testdb
+
+import "strings"
+
+type TestDB struct {
+	db map[string]string
+}
+
+func NewTestDB() *TestDB {
+	return &TestDB{
+		db: make(map[string]string),
+	}
+}
+
+func (t *TestDB) Set(json string) error {
+	t.db[kay(json)] = json
+	return nil
+}
+
+func (t *TestDB) List() ([]string, error) {
+	var list []string
+	for _, v := range t.db {
+		list = append(list, v)
+	}
+	return list, nil
+}
+
+func kay(json string) string {
+	return strings.Trim(strings.SplitAfterN(json, "\"", 5)[3], "\"")
+}
--- go.mod
+++ go.mod
@@ -13,7 +13,9 @@ require (
 	github.com/nats-io/nats.go v1.27.1 // indirect
 	github.com/nats-io/nkeys v0.4.4 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/ramya-rao-a/go-outline v0.0.0-20210608161538-9736a4bde949 // indirect
 	golang.org/x/crypto v0.10.0 // indirect
 	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/tools v0.1.12 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
 )
