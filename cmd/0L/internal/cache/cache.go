203ec52c5c0dba0d499059f3b0eeea4c0473c352
MTimofei <work.tim@rambler.ru>
2023-07-20 19:37:09 +0300

    create cache interface

--- cmd/0L/internal/cache/cache.go
+++ cmd/0L/internal/cache/cache.go
@@ -1,1 +1,19 @@
 package cache
+
+type Cache interface {
+	Set(Payload) error
+	SetList([]Payload) error
+	Single(kay string) (Payload, error)
+}
+
+type Payload struct {
+	Key   string
+	Value []byte
+}
+
+func NewPayload(key string, value []byte) Payload {
+	return Payload{
+		Key:   key,
+		Value: value,
+	}
+}
