d1193000883b991b4e7e70ac7eb9ce37a0cac130
MTimofei <work.tim@rambler.ru>
2023-07-21 14:11:24 +0300

    test app checked

--- cmd/0L/main.go
+++ cmd/0L/main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"github.com/wb/cmd/0L/internal/server"
+	"github.com/wb/cmd/0L/internal/serves"
 )
 
 func main() {
+	if err := serves.App.Start(); err != nil {
+		panic(err)
+	}
+
+	go serves.App.Work()
+
 	if err := server.NewServer().Run(); err != nil {
 		panic(err)
 	}
